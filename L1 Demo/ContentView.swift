50c18bf4e5a5521abd40ada06543d8ac2598f136
Martha <marthamengesha78@gmail.com>
2025-12-07 17:53:19 -0500

    Initial Commit

---
+++ L1 Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  L1 Demo
+//
+//  Created by Martha Mengesha on 2025-12-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
