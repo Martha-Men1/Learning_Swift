fde6e56d9fc96e43c75fc2ea77576ef2068d9002
Martha <marthamengesha78@gmail.com>
2025-12-10 13:02:19 -0500

    Add WeatherForecast as normal folder

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Martha Mengesha on 2025-12-10.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
